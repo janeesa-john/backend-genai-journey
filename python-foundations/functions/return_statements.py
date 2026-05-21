6baa8e6ce159266e409bf9222ac2410ebc3faf90
janeesa-john <janeesajon@gmail.com>
2026-05-21 13:27:02 +0530

    Implemented practical examples demonstrating Python return statements

---
+++ python-foundations/functions/return_statements.py
@@ -0,0 +1,38 @@
+# Using print()
+
+def calculate_bill(price1, price2):
+
+    total = price1 + price2
+
+    print("Total:", total)
+
+calculate_bill(100, 200)
+
+# Using return
+
+def calculate_bill(price1, price2):
+
+    total = price1 + price2
+
+    return total
+
+
+result = calculate_bill(100, 200)
+
+print("Total:", result)
+
+# Reusing returned value
+
+def calculate_marks(mark1, mark2, mark3):
+
+    total = mark1 + mark2 + mark3
+
+    return total
+
+
+result = calculate_marks(78, 85, 90)
+
+average = result / 3
+
+print("Total Marks:", result)
+print("Average:", average)
